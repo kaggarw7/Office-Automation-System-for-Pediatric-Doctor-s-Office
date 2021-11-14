0a326530dd04682ecdb0f243f7201e64bbb4adf6
Nicholas Fowler <nicholasfowler@protonmail.com>
2021-11-13 21:13:26 -0700

    Started rewrite of Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,16 +1,37 @@
-import java.util.*;
-public class Doctor {
-	
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Doctor extends Person {
 	private Nurse assignedNurse;
-	private Patient assignedPatient[];
-	
-	public Nurse getAssignedNurse()
-	{
-		return this.assignedNurse;
-	}
-	
-	public Patient getPatient(int index)
-	{
-		return this.assignedPatient[index];
-	}
+	private ArrayList<Patient> assignedPatients;
+
+	// Constructor
+
+    public Doctor(String firstName, String lastName, LocalDate birthday, Nurse assignedNurse) {
+        super(firstName, lastName, birthday);
+
+        this.assignedNurse = assignedNurse;
+    }
+
+    // Getters
+
+    public Nurse getAssignedNurse() {
+        return assignedNurse;
+    }
+
+    public ArrayList<Patient> getAssignedPatients() {
+        return assignedPatients;
+    }
+
+    // Setters
+
+    public void setAssignedNurse(Nurse assignedNurse) {
+        this.assignedNurse = assignedNurse;
+    }
+
+    // Functionality
+
+    public void addNewPatient(Patient patient) {
+        assignedPatients.add(patient);
+    }
 }
