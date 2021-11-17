beade66bb7a65badf6f06ff94db31fd677c52fa5
nfowler <49357854+nfowler@users.noreply.github.com>
2021-11-16 22:14:54 -0700

    Reupload of Prescription.java

---
+++ Prescription.java
@@ -0,0 +1,35 @@
+public class Prescription {
+    private String medicine;
+    private String dosageAmount;
+    private int timesPerMonth;
+
+    // Constructor
+
+    public Prescription(String medicine, String dosageAmount, int timesPerMonth) {
+        this.medicine = medicine;
+        this.dosageAmount = dosageAmount;
+        this.timesPerMonth = timesPerMonth;
+    }
+
+    // Getters
+
+    public String getMedicine() {
+        return medicine;
+    }
+
+    public String getDosageAmount() {
+        return dosageAmount;
+    }
+
+    public int getTimesPerMonth() {
+        return timesPerMonth;
+    }
+
+    // Functionality
+
+    @Override
+    public String toString() {
+        return this.medicine + " / " + this.dosageAmount + " / " + this.timesPerMonth;
+    }
+
+}
